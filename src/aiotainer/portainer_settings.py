8b475370bf1b072d76e873e656b615ae434ac7af
Thomas55555 <thomas.protzner@gmail.com>
2024-10-28 21:51:23 +0100

    ruffy

--- src/aiotainer/portainer_settings.py
+++ src/aiotainer/portainer_settings.py
@@ -140,6 +140,7 @@ seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
 
 
 def convert_to_seconds(s: str) -> int:
+    """Convert string to seconds."""
     return int(s[:-1]) * seconds_per_unit[s[-1]]
 
 
